cc9aaa45fee5a9de85b3426f4650642c8517305f
aisi <aisi.hhf@xuelangyun.com>
2020-09-01 09:25:49 +0800

    fix

--- apps/service/src/app.controller.ts
+++ apps/service/src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MathService } from '@libs/math/math.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly mathService: MathService
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/sum')
+  getSum(): number {
+    return this.mathService.sum(1, 2);
   }
 }
--- apps/service/src/app.service.ts
+++ apps/service/src/app.service.ts
@@ -1,17 +1,8 @@
-import { Get, Injectable } from '@nestjs/common';
-import { MathService } from '@libs/math/math.service';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  constructor(
-    private readonly mathService: MathService
-  ) {}
   getHello(): string {
     return 'Hello World!';
-  }
-
-  @Get('/sum')
-  getSum(): number {
-    return this.mathService.sum(1, 2);
   }
 }
